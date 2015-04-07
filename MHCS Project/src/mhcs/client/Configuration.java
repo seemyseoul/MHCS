c921edaabb0297cd5848c05c1de3fcafd30dd236
John McKeown <jackamckeown@gmail.com>
2015-04-07 16:48:55 -0500

    defined the interface for the Configurations class

--- MHCS Project/src/mhcs/client/Configuration.java
+++ MHCS Project/src/mhcs/client/Configuration.java
@@ -3,12 +3,31 @@ package mhcs.client;
 
 public class Configuration {
 
+	private Module[] modules;
+	
 	public Configuration(Module[] m)
 	{
-		
+		this.modules = m;
 	}
 	
-	public getRoverPath()
+	public Module[] getModules()
+	{
+		return modules;
+	}
+	
+	public void setModules(Modules[] m)
+	{
+		modules = m;
+	}
+	
+	/** this method returns the rover path as a Point[].
+	 *  It has to take in an array of the original modules
+	 *  so that it can compare unconfigured and configured module locations
+	 *  to generate the most efficient path.
+	 *
+	 * @return Point[] path
+	 */
+	public Point[] getRoverPath(Module[] m)
 	{
 		
 	}
