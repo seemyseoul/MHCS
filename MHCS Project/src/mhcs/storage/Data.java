16ffcd90901bbaff793f8289f20f84de2773bd1f
Brent <brentr678@outlook.com>
2015-04-14 15:10:49 -0500

    changed data

--- MHCS Project/src/mhcs/storage/Data.java
+++ MHCS Project/src/mhcs/storage/Data.java
@@ -7,43 +7,32 @@ import mhcs.client.Configuration;
 import mhcs.client.Module;
 
 public class Data {
-
-	public Data(){
-		toLoad = new Load();
-		toSave = new Save();
-		moduleList = toLoad.getModuleList();
-		configList = toLoad.getConfigurationList();
-		password = toLoad.getPassword();
-		passwordFlag = toLoad.getPasswordFlag();
 		
-	}
-	
-	
-	public void addModule(Module toAdd){
+	public static void addModule(Module toAdd){
 		moduleList.add(toAdd);
 		toSave.saveModules(moduleList);
 	}
 	
-	public void addConfiguraiton(Configuration toAdd){
+	public static void addConfiguraiton(Configuration toAdd){
 		configList.add(toAdd);
 		toSave.saveConfigurations(configList);
 	}
 	
 	
-	public void removeModule(Module toRemove){
+	public static void removeModule(Module toRemove){
 		moduleList.remove(findListNumber(toRemove));
 		toSave.removeAllModules();
 		toSave.saveModules(moduleList);
 	}
 	
-	public void removeConfiguration(Configuration toRemove){
+	public static void removeConfiguration(Configuration toRemove){
 		configList.remove(findListNumber(toRemove));
 		toSave.removeAllConfigurations();
 		toSave.saveConfigurations(configList);
 	}
 	
 	
-	public void editModule(Module toEdit){
+	public static void editModule(Module toEdit){
 		moduleList.remove(findListNumber(toEdit));
 		toSave.removeAllModules();
 		toSave.saveModules(moduleList);
@@ -51,7 +40,7 @@ public class Data {
 		toSave.saveModules(moduleList);
 	}
 	
-	public void editConfiguration(Configuration toEdit){
+	public static void editConfiguration(Configuration toEdit){
 		configList.remove(findListNumber(toEdit));
 		toSave.removeAllConfigurations();
 		toSave.saveConfigurations(configList);
@@ -64,24 +53,24 @@ public class Data {
 	
 	
 	
-	public void updateFlag(boolean flag){
+	public static void updateFlag(boolean flag){
 		passwordFlag = flag;
 		toSave.saveSettings(flag, password);
 	}
 	
-	public void updatePassword(String newPassword){
+	public static void updatePassword(String newPassword){
 		password = newPassword;
 		toSave.saveSettings(passwordFlag, newPassword);
 	}
 	
-	public void addConfiguration(Configuration toAdd){
+	public static void addConfiguration(Configuration toAdd){
 		configList.add(toAdd);
 		toSave.saveConfigurations(configList);
 	}
 
 	
 	
-	private int findListNumber(Module module){
+	private static int findListNumber(Module module){
 		for(int i = 0; i < moduleList.size(); i++){
 			Module compare = moduleList.get(i);
 			if(module.getId() == compare.getId())
@@ -90,7 +79,7 @@ public class Data {
 		return -1;
 	}
 	
-	private int findListNumber(Configuration config){
+	private static int findListNumber(Configuration config){
 		for(int i = 0; i < configList.size(); i++){
 			Configuration compare = configList.get(i);
 			if(config.getId() == compare.getId())
@@ -106,48 +95,48 @@ public class Data {
 	/**
 	 * @return the moduleList
 	 */
-	public List<Module> getModuleList() {
+	public static List<Module> getModuleList() {
 		return moduleList;
 	}
 	/**
 	 * @param moduleList the moduleList to set
 	 */
-	public void setModuleList(List<Module> moduleList) {
-		this.moduleList = moduleList;
+	public static void setModuleList(List<Module> moduleListParam) {
+		moduleList = moduleListParam;
 	}
 	/**
 	 * @return the password
 	 */
-	public String getPassword() {
+	public static String getPassword() {
 		return password;
 	}
 	/**
 	 * @param password the password to set
 	 */
-	public void setPassword(String password) {
-		this.password = password;
+	public static void setPassword(String passwordPram) {
+		password = passwordPram;
 	}
 	/**
 	 * @return the passwordFlag
 	 */
-	public boolean isPasswordFlag() {
+	public static boolean isPasswordFlag() {
 		return passwordFlag;
 	}
 	/**
 	 * @param passwordFlag the passwordFlag to set
 	 */
-	public void setPasswordFlag(boolean passwordFlag) {
-		this.passwordFlag = passwordFlag;
+	public static void setPasswordFlag(boolean passwordFlagParam) {
+		passwordFlag = passwordFlagParam;
 	}
 
 
 
 
 
-	private Load toLoad;
-	private Save toSave;
-	private List<Module> moduleList = new ArrayList<>();
-	private List<Configuration> configList = new ArrayList<>();
-	private String password = "Naples";
-	private boolean passwordFlag = true; 
+	private static Load toLoad = new Load();
+	private static Save toSave = new Save();
+	private static List<Module> moduleList = toLoad.getModuleList();
+	private static List<Configuration> configList = toLoad.getConfigurationList();
+	private static String password = toLoad.getPassword();
+	private static boolean passwordFlag = toLoad.getPasswordFlag();
 }
