75d020d2e20a1e9ec741d7e3ffcdc54244117068
Amanda <posto010@d.umn.edu>
2015-04-15 12:11:10 -0500

    Removed most checkstyle things in Data.java

--- MHCS Project/src/mhcs/storage/Data.java
+++ MHCS Project/src/mhcs/storage/Data.java
@@ -9,126 +9,134 @@ import mhcs.client.ModuleType;
 import mhcs.client.Point;
 
 public class Data {
-		
-	public static void addModule(Module toAdd){
-		moduleList.add(toAdd);
-		toSave.saveModules(moduleList);
-	}
-	
-	public static void addConfiguraiton(Configuration toAdd){
-		configList.add(toAdd);
-		toSave.saveConfigurations(configList);
-	}
-	
-	
-	public static void removeModule(Module toRemove){
-		moduleList.remove(findListNumber(toRemove));
-		toSave.removeAllModules();
-		toSave.saveModules(moduleList);
-	}
-	
-	public static void removeConfiguration(Configuration toRemove){
-		configList.remove(findListNumber(toRemove));
-		toSave.removeAllConfigurations();
-		toSave.saveConfigurations(configList);
-	}
-	
-	
-	public static void editModule(Module toEdit){
-		moduleList.remove(findListNumber(toEdit));
-		toSave.removeAllModules();
-		toSave.saveModules(moduleList);
-		moduleList.add(toEdit);
-		toSave.saveModules(moduleList);
-	}
-	
-	public static void editConfiguration(Configuration toEdit){
-		configList.remove(findListNumber(toEdit));
-		toSave.removeAllConfigurations();
-		toSave.saveConfigurations(configList);
-		configList.add(toEdit);
-		toSave.saveConfigurations(configList);
-	}
-	
-	public static void updateFlag(boolean flag){
-		passwordFlag = flag;
-		toSave.saveSettings(flag, password);
-	}
-	
-	public static void updatePassword(String newPassword){
-		password = newPassword;
-		toSave.saveSettings(passwordFlag, newPassword);
-	}
-	
-	public static void addConfiguration(Configuration toAdd){
-		configList.add(toAdd);
-		toSave.saveConfigurations(configList);
-	}
 
-	
-	
-	private static int findListNumber(Module module){
-		for(int i = 0; i < moduleList.size(); i++){
-			Module compare = moduleList.get(i);
-			if(module.getId() == compare.getId())
-				return i;
-		}
-		return -1;
-	}
-	
-	private static int findListNumber(Configuration config){
-		for(int i = 0; i < configList.size(); i++){
-			Configuration compare = configList.get(i);
-			if(config.getId() == compare.getId())
-				return i;
-		}
-		return -1;
-	}	
-	
-	
-	
-	
+  public static void addModule(Module toAdd) {
+    moduleList.add(toAdd);
+    toSave.saveModules(moduleList);
+  }
+  
+  public static void addConfiguraiton(Configuration toAdd) {
+    configList.add(toAdd);
+    toSave.saveConfigurations(configList);
+  }
 
-	/**
-	 * @return the moduleList.
-	 * (It should return a copy as to not allow changes to be made accidentally)
-	 */
-	@SuppressWarnings("unchecked")
-	public static List<Module> getModuleList() {
-		ArrayList<Module> moduleList1 = new ArrayList<Module>(moduleList);
-		return (List<Module>) moduleList1.clone();
-	}
-	/**
-	 * @param moduleList the moduleList to set
-	 */
-	public static void setModuleList(List<Module> moduleListParam) {
-		moduleList = moduleListParam;
-		// THIS METHOD SHOULD ALSO UPDATE THE HTML5 LOCALSTORAGE.
-	}
-	/**
-	 * @return the password
-	 */
-	public static String getPassword() {
-		return password;
-	}
-	/**
-	 * @param password the password to set
-	 */
-	public static void setPassword(String passwordPram) {
-		password = passwordPram;
-	}
-	/**
-	 * @return the passwordFlag
-	 */
-	public static boolean isPasswordFlag() {
-		return passwordFlag;
-	}
-	/**
-	 * @param passwordFlag the passwordFlag to set
-	 */
-	public static void setPasswordFlag(boolean passwordFlagParam) {
-		passwordFlag = passwordFlagParam;
-	}
+
+  public static void removeModule(Module toRemove) {
+    moduleList.remove(findListNumber(toRemove));
+    toSave.removeAllModules();
+    toSave.saveModules(moduleList);
+  }
+
+  public static void removeConfiguration(Configuration toRemove) {
+    configList.remove(findListNumber(toRemove));
+    toSave.removeAllConfigurations();
+    toSave.saveConfigurations(configList);
+  }
+
+  public static void editModule(Module toEdit) {
+    moduleList.remove(findListNumber(toEdit));
+    toSave.removeAllModules();
+    toSave.saveModules(moduleList);
+    moduleList.add(toEdit);
+    toSave.saveModules(moduleList);
+  }
+
+  public static void editConfiguration(Configuration toEdit) {
+    configList.remove(findListNumber(toEdit));
+    toSave.removeAllConfigurations();
+    toSave.saveConfigurations(configList);
+    configList.add(toEdit);
+    toSave.saveConfigurations(configList);
+  }
+
+  public static void updateFlag(boolean flag) {
+    passwordFlag = flag;
+    toSave.saveSettings(flag, password);
+  }
+
+  public static void updatePassword(String newPassword) {
+    password = newPassword;
+    toSave.saveSettings(passwordFlag, newPassword);
+  }
+
+  public static void addConfiguration(Configuration toAdd) {
+    configList.add(toAdd);
+    toSave.saveConfigurations(configList);
+  }
+
+  private static int findListNumber(Module module) {
+    for (int i = 0; i < moduleList.size(); i++) {
+      Module compare = moduleList.get(i);
+      if (module.getId() == compare.getId()) {
+        return i;
+      } // if
+    } // for
+    return -1;
+  }
+
+  private static int findListNumber(Configuration config) {
+    for (int i = 0; i < configList.size(); i++) {
+      Configuration compare = configList.get(i);
+      if (config.getId() == compare.getId()) {
+        return i;
+      } // if
+      return -1;
+    } // for
+    return -1;
+  }
+
+
+  /**
+   * Returns the module list.
+   * @return the moduleList.
+   * (It should return a copy as to not allow changes to be made accidentally)
+   */
+  @SuppressWarnings("unchecked")
+  public static List<Module> getModuleList() {
+    ArrayList<Module> moduleList1 = new ArrayList<Module>(moduleList);
+    return (List<Module>) moduleList1.clone();
+  }
+  
+  /**
+   * Returns the module list.
+   * @param moduleList the moduleList to set
+   */
+  public static void setModuleList(List<Module> moduleListParam) {
+    moduleList = moduleListParam;
+    // THIS METHOD SHOULD ALSO UPDATE THE HTML5 LOCALSTORAGE.
+  }
+
+  /**
+   * Returns the password.
+   * @return the password
+   */
+  public static String getPassword() {
+    return password;
+  }
+  
+  /**
+   * Changes the password.
+   * @param password the password to set
+   */
+  public static void setPassword(String passwordPram) {
+    password = passwordPram;
+  }
+
+  /**
+   * Returns the password flag.
+   * @return the passwordFlag
+   */
+  public static boolean isPasswordFlag() {
+    return passwordFlag;
+  }
+
+  /**
+   * Sets the password flag.
+   * @param passwordFlag the passwordFlag to set
+   */
+  public static void setPasswordFlag(boolean passwordFlagParam) {
+    passwordFlag = passwordFlagParam;
+  }
 
 
 
