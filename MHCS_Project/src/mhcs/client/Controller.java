c04856c9c1a90a8df65ca8f0c587695c998e5871
Amanda <posto010@d.umn.edu>
2015-04-21 15:42:15 -0500

    Started implementing the setType

--- MHCS_Project/src/mhcs/client/Controller.java
+++ MHCS_Project/src/mhcs/client/Controller.java
@@ -1,5 +1,30 @@
 package mhcs.client;
 
-public class Controller {
+import com.google.gwt.user.client.ui.TextBox;
 
-}
+public class Controller {
+	/**
+	 * Generic empty constructor for controller.
+	 */
+	public Controller() {}
+	
+	public void setType(TextBox id) {
+		String modId = id.getText();
+		int intModId = Integer.parseInt(modId);
+		if (0 < intModId && intModId < 41) {
+			id.setValue("Air Lock");
+		} // if
+		else if (60 < intModId && intModId < 81) {
+			id.setValue("Dormitory");
+		} // if
+		else if (90 < intModId && intModId < 101) {
+			id.setValue("Sanitation");
+		} // if
+		else if (110 < intModId && intModId < 121) {
+			id.setValue("Dormitory");
+		} // if
+		
+		
+	} // setType
+	
+} // Controller
