6a2ec7325904b19dbd9098b6d69b129e01dd269e
Amanda <posto010@d.umn.edu>
2015-04-30 13:00:34 -0500

    Added sounds to the index

--- MHCS_Project/src/mhcs/client/Variables.java
+++ MHCS_Project/src/mhcs/client/Variables.java
@@ -536,7 +536,6 @@ public class Variables {
     
     public static TextBox tbTimeSince() {
         timeSince.setEnabled(false);
-//      timeSince.setText(Controller.timeSince());
         return timeSince;
     } // timeSince
     
