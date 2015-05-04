40570120ff7da6882e992ab2a12949389c8f08b1
Amanda <posto010@d.umn.edu>
2015-05-03 21:57:24 -0500

    Changes to Map class.

--- MHCS_Project/src/mhcs/client/Map.java
+++ MHCS_Project/src/mhcs/client/Map.java
@@ -1,10 +1,15 @@
 package mhcs.client;
 
+import java.util.List;
+
 import com.google.gwt.user.client.ui.Grid;
+import com.google.gwt.user.client.ui.Image;
+import com.google.gwt.user.client.ui.TextBox;
 
 public class Map {
 	/**
 	 * Constructor
+	 * @return 
 	 */
 	public Map() {
 		Grid g = new Grid(50, 100);
@@ -15,5 +20,20 @@ public class Map {
 	      } // for
 	    } // for
 	} // ctor
+	
+	public void placeModule(Module module) {
+		Map map = new Map();
+		Point coordinates = module.getCoordinates();
+		int x = coordinates.getX();
+		int y = coordinates.getY();
+		map.setWidget(x, y, module.getImage());
+	} // placeModule
 
+	public void placeConfiguration(Map map, Configuration config) {
+		List<Module> modules = config.getModules();
+		for (int i=0; i < modules.size(); i++) {
+			map.placeModule(modules.get(i));
+		} // for
+	} // placeConfiguration
+	
 } // Map
