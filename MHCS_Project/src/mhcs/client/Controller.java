3c5aa60ce13ae71edc3621aa683fde7e4b0fb2c5
Brent <brentr678@outlook.com>
2015-05-04 20:17:03 -0500

    Controller

--- MHCS_Project/src/mhcs/client/Controller.java
+++ MHCS_Project/src/mhcs/client/Controller.java
@@ -705,7 +705,7 @@ public class Controller {
 	 * @param lb
 	 * @return ChangeHandler
 	 */
-	public static ChangeHandler testCases(final ListBox lb){
+	public static ChangeHandler testCases(final ListBox lb, final Map map){
 		return new ChangeHandler() {
 			public void onChange(ChangeEvent event) {
 				int testNum = lb.getSelectedIndex();
@@ -718,6 +718,12 @@ public class Controller {
 					Variables.mListBox().clear();
 					Variables.cListBox().clear();
 				}
+				
+				List<Module> modules = Model.getModuleList();
+				for (int i = 0; i < modules.size(); i++) {
+					map.placeModules(map, modules.get(i));
+				} // for				
+				
 			} //onChange
 		};
 	} // testCases
