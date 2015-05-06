0d4cd2261fe12f7dafd6cea2357eef705fabc340
Brent <brentr678@outlook.com>
2015-05-05 20:41:29 -0500

    map

--- MHCS_Project/src/mhcs/client/Map.java
+++ MHCS_Project/src/mhcs/client/Map.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import mhcs.storage.Model;
 
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Grid;
 import com.google.gwt.user.client.ui.Image;
 
@@ -66,18 +67,14 @@ public class Map {
 				configMod = configModules.get(i);
 				freeMod = freeModules.get(j);
 				if (configMod.getId() == freeMod.getId()){
+					Window.alert("MATCH FOUND");
 					Point coordinates = freeMod.getCoordinates();
 					int x = translateX(coordinates.getX());
 					int y = translateY(coordinates.getY());
-					map.setWidget(y, x, makeOpaque(freeMod));					
+					Variables.map.setWidget(y, x, makeOpaque(freeMod));					
 				}//if
 			}//for
 		}//for
-		
-		
-	//update to appease git	
-		
-		
 	}//changePic	
 	
 	
@@ -93,66 +90,56 @@ public class Map {
 	
 	public Image makeOpaque(Module modType) { 
 		int intModId = modType.getId();
-		Image modImage = new Image("images/blank");
+		Image modImage = new Image("images/plain_used.jpg");
 		if (0 < intModId && intModId < 41) {
 			modImage = new Image("images/modules/plain_used.jpg");
 			modImage.setHeight(Variables.px150);
 			modImage.setWidth(Variables.px150);
-			Variables.m.add(modImage);
 		} // if
 		else if (60 < intModId && intModId < 81) {
 			modImage = new Image("images/modules/dormitory_used.jpg");
 			modImage.setHeight(Variables.px150);
 			modImage.setWidth(Variables.px150);
-			Variables.m.add(modImage);
 		} // if
 		else if (90 < intModId && intModId < 101) {
 			modImage = new Image("images/modules/sanitation_used.jpg");
 			modImage.setHeight(Variables.px150);
 			modImage.setWidth(Variables.px150);
-			Variables.m.add(modImage);
 		} // if
 		else if (110 < intModId && intModId < 121) {
 			modImage = new Image("images/modules/foodWaterStorage_used.jpg");
 			modImage.setHeight(Variables.px150);
 			modImage.setWidth(Variables.px150);
-			Variables.m.add(modImage);
 		} // if
 		else if (130 < intModId && intModId < 135) {
 			modImage = new Image("images/modules/gymRelaxation_used.jpg");
 			modImage.setHeight(Variables.px150);
 			modImage.setWidth(Variables.px150);
-			Variables.m.add(modImage);
 		} // if
 		else if (140 < intModId && intModId < 145) {
 			modImage = new Image("images/modules/canteen_used.jpg");
 			modImage.setHeight(Variables.px150);
 			modImage.setWidth(Variables.px150);
-			Variables.m.add(modImage);
 		} // if
 		else if (150 < intModId && intModId < 155) {
 			modImage = new Image("images/modules/power_used.jpg");
 			modImage.setHeight(Variables.px150);
 			modImage.setWidth(Variables.px150);
-			Variables.m.add(modImage);
 		} // if
 		else if (160 < intModId && intModId < 165) {
 			modImage = new Image("images/modules/control_used.jpg");
 			modImage.setHeight(Variables.px150);
 			modImage.setWidth(Variables.px150);
-			Variables.m.add(modImage);
 		} // if
 		else if (170 < intModId && intModId < 175) {
 			modImage = new Image("images/modules/airlock_used.jpg");
 			modImage.setHeight(Variables.px150);
 			modImage.setWidth(Variables.px150);
-			Variables.m.add(modImage);
 		} // if
 		else if (180 < intModId && intModId < 185) {
 			modImage = new Image("images/modules/medical_used.jpg");
 			modImage.setHeight(Variables.px150);
 			modImage.setWidth(Variables.px150); 
-			Variables.m.add(modImage);
 		} // if
 		return modImage;
 	}
