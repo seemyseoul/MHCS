dc2a3a2f23fe317fdcb2be66a048e694fa1abea2
Brent <brentr678@outlook.com>
2015-05-05 19:54:38 -0500

    controller

--- MHCS_Project/src/mhcs/client/Controller.java
+++ MHCS_Project/src/mhcs/client/Controller.java
@@ -480,7 +480,8 @@ public class Controller {
 			@Override
 			public void onChange(ChangeEvent event) {
 				Variables.map.clearMap();
-//				Variables.map.changePic(Model.getConfigList().get(Variables.cListBox.getSelectedIndex()), Variables.map);				
+				Window.alert("HERE BRENT!");				
+				Variables.map.changePic(Model.getConfigList().get(Variables.cListBox.getSelectedIndex()), Variables.map);				
 				Variables.map.placeConfiguration(Variables.map,Model.getConfigList().get(Variables.cListBox.getSelectedIndex()));
 			}
 		};
