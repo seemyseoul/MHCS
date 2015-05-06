4721561de5e8bcd2f26ff50e6982d729fc0c591d
John McKeown <jackamckeown@gmail.com>
2015-05-05 22:41:14 -0500

    Variables

--- MHCS_Project/src/mhcs/client/Variables.java
+++ MHCS_Project/src/mhcs/client/Variables.java
@@ -211,7 +211,7 @@ public class Variables {
 					m.setCoordinates(new Point(m.getCoordinates().getX()+dx,m.getCoordinates().getY()+dy));
 				}
 				Variables.map.clearMap();
-				//Window.alert("STUFF");
+				Window.alert("STUFF");
 				List<Configuration> configLista = Model.getConfigList();
 				configLista.remove(Variables.cListBox().getSelectedIndex());
 				configLista.add(Variables.cListBox().getSelectedIndex(),c);
